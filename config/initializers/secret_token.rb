8998be5aa1ef095ac0eca7f4382d74d08e1dd146
Joseph L <josephtwli@berkeley.edu>
2013-12-24 23:35:52 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+APIMashup::Application.config.secret_key_base = '7a4c53a3eed0447e23b0e6e7941d193220c786c7c61005687b18a41b468cc7374885cbfff6ed65a12cd925c012c26b7371313e1d06f0207c87138d51d78ca32f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+APIMashup::Application.config.session_store :cookie_store, key: '_API_Mashup_session'
